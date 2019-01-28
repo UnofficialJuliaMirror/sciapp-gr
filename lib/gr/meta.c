fad2d5af274e996a67de2220bb67f7e48a2e6ba2
Jonas Clever <j.clever@fz-juelich.de>
2019-01-28 09:04:31 +0100

    Updated the `next_or_equal_power2` method

--- lib/gr/meta.c
+++ lib/gr/meta.c
@@ -23,6 +23,9 @@
 #include <sys/socket.h>
 #include <unistd.h>
 #endif
+#ifdef _MSC_VER
+#include <intrin.h>
+#endif
 
 #include "gks.h"
 #include "gr.h"
@@ -3865,12 +3868,21 @@ int uppercase_count(const char *str) {
 
 unsigned long next_or_equal_power2(unsigned long num) {
 #if defined(__GNUC__) || defined(__clang__)
-  return 1ul << ((CHAR_BIT * sizeof(unsigned long)) - __builtin_clzl(num));
+  /* Subtract the count of leading bit zeros from the count of all bits to get `floor(log2(num)) + 1`. If `num` is a
+   * power of 2 (only one bit is set), subtract 1 more. The result is `ceil(log2(num))`. Calculate
+   * `1 << ceil(log2(num))` to get `exp2(ceil(log2(num)))` which is the power of 2 which is greater or equal than `num`.
+   */
+  return 1ul << ((CHAR_BIT * sizeof(unsigned long)) - __builtin_clzl(num) - (__builtin_popcountl(num) == 1 ? 1 : 0));
 #elif defined(_MSC_VER)
+  /* Calculate the index of the highest set bit (bit scan reverse) to get `floor(log2(num)) + 1`. If `num` is a power
+   * of 2 (only one bit is set), subtract 1 more. The result is `ceil(log2(num))`. Calculate `1 << ceil(log2(num))`
+   * to get `exp2(ceil(log2(num)))` which is the power of 2 which is greater or equal than `num`.
+   */
   unsigned long index;
   _BitScanReverse(&index, num);
-  return 1ul << index;
+  return 1ul << (index + 1 - (__popcnt(num) == 1 ? 1 : 0));
 #else
+  /* Fallback algorithm in software: Shift one bit till the resulting number is greater or equal than `num` */
   unsigned long power = 1;
   while (power < num) {
     power <<= 1;
